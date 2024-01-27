e0881e8f6390e4443d7c02ced14420c06453e2eb
Aleksandar Bobic <aleksandar.bobic@brightmarbles.io>
2024-01-27 21:00:54 +0100

    ...

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -93,43 +93,6 @@ const mainmenu: NavigationMenuItem[] = [
   },
 ];
 
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-];
 
 export function Navigation() {
   return (
@@ -139,7 +102,7 @@ export function Navigation() {
           <NavigationMenuItem key={item.title}>
             {item.subitems ? (
               <>
-                <NavigationMenuTrigger>Components</NavigationMenuTrigger>
+                <NavigationMenuTrigger>{item.title}</NavigationMenuTrigger>
                 <NavigationMenuContent>
                   <ul className="grid w-[430px] gap-3 p-4">
                     {item.subitems.map((component) => (
