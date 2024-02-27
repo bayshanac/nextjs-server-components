a7c0141a79f82ba6cbc9bca88bb2fe3272a40458
Aleksandar Bobic <aleksandar.bobic@brightmarbles.io>
2024-02-27 16:26:53 +0100

    ...

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,19 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getErrorMessage = (error: unknown): string => {
+  if (error instanceof Error) {
+    return error.message;
+  }
+  if (error && typeof error === "object" && "message" in error) {
+    return String(error.message);
+  }
+  if (typeof error === "string") {
+    return error;
+  }
+  return "Something went wrong";
+};
